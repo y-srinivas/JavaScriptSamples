653f33fbef39bacde149ba8b9a993c6cef901b64
srinivas Y <Srinivas.Yellapantula@voya.com>
2017-08-31 21:03:55 -0400

    Intial

--- Sample1/main.js
+++ Sample1/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by Srinivas Y  on 08/31/2017.
  */
-//Implicit "this" reference
+
 function foo() {
 console.log( this.a );
 }
@@ -10,16 +10,9 @@ var obj = {
     a: 2
 };
 
+//Implicit "this" reference
 foo( obj ); // undefined
 
 
 //Explicit "this" reference
-function foo() {
-    console.log( this.a );
-}
-
-var obj = {
-    a: 2
-};
-
 foo.call( obj ); // 2

be8a247b4149942cd2620337699a0c5793f2e944
srinivas Y <Srinivas.Yellapantula@voya.com>
2017-08-31 21:02:31 -0400

    Intial

--- Sample1/main.js
+++ Sample1/main.js
@@ -1,5 +1,25 @@
 /**
  * Created by Srinivas Y  on 08/31/2017.
  */
-var a = 5;
-console.log(a);
+//Implicit "this" reference
+function foo() {
+console.log( this.a );
+}
+
+var obj = {
+    a: 2
+};
+
+foo( obj ); // undefined
+
+
+//Explicit "this" reference
+function foo() {
+    console.log( this.a );
+}
+
+var obj = {
+    a: 2
+};
+
+foo.call( obj ); // 2
